e5bd96c184def5bc2a4cd77b7ff2417167583573
avats <avats@griddynamics.com>
2024-06-17 12:27:41 +0530

    spring boot backend for voice based authentication

--- src/main/java/com/griddynamics/capstone_project/service/AudioAuthService.java
+++ src/main/java/com/griddynamics/capstone_project/service/AudioAuthService.java
@@ -2,6 +2,7 @@ package com.griddynamics.capstone_project.service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,7 +20,7 @@ public class AudioAuthService {
         this.webClient = webClientBuilder.baseUrl("http://localhost:8000").build(); //fastAPI Bbase url
     }
 
-    public String predictUser(MultipartFile file) throws Exception {
+    public String predictUser(@NonNull MultipartFile file) throws Exception { //enable better exception handling
         return webClient.post()
                 .uri("/predict/")
                 .body(BodyInserters.fromMultipartData("file", file.getResource()))
