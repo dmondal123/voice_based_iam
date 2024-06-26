4ffadd36ea4eeb49a20ab59ac0fbfcb009c2e923
Anushka Singh <anusingh@griddynamics.com>
2024-06-26 21:16:29 +0530

    After adding timer and removing latency, latest frontend code for voice login

--- src/components/AudioRecorder.tsx
+++ src/components/AudioRecorder.tsx
@@ -1,19 +1,16 @@
-
 import React, { useEffect, useState } from "react";
 import { useAudioRecorder } from "react-audio-voice-recorder";
 import micIcon from "../../public/mic.svg";
-import stopIcon from "../../public/pause_button.svg"
+import stopIcon from "../../public/pause_button.svg";
 import Waves from "./Waves";
 import VerifyPrompt from "./VerifyPrompt";
 
-const AudioRecorderComponent = ({activateLogin}) => {
+const AudioRecorderComponent = ({ activateLogin }) => {
   const {
     startRecording,
     stopRecording,
-    togglePauseResume,
     recordingBlob,
     isRecording,
-    isPaused,
     recordingTime,
   } = useAudioRecorder({
     audioTrackConstraints: {
@@ -25,98 +22,88 @@ const AudioRecorderComponent = ({activateLogin}) => {
     },
   });
 
-  const [verify,setVerify] = useState({predicted_speaker:"Anushka"});
+  const [verify, setVerify] = useState({});
   const [result, setResult] = useState(false);
-  const [myrecording, setMyRecording] = useState(false);
+  const [myRecording, setMyRecording] = useState(false);
 
- 
-// console.log('recordingBlob=>',startRecording)
+
   useEffect(() => {
     if (recordingBlob) {
-      console.log(recordingBlob);
-      
-      // const url = URL.createObjectURL(recordingBlob);
-      // const audio = document.createElement("audio");
-      // audio.src = url;
-      // audio.controls = true;
-      // document.body.appendChild(audio);
-    }
+      console.log("Valid Recording Blob: ", recordingBlob);
 
-    const sendAudioToBackend = async () => {
-      console.log('stop Recording')
-      if (recordingBlob) {
+      const sendAudioToBackend = async () => {
         setResult(true);
         setMyRecording(true);
 
-        if(verify.predicted_speaker!=="other" && verify.predicted_speaker!=="Cloned" && verify.predicted_speaker!=="No Voice")
-            {
-              activateLogin();
-            }
         
-        
+
         const formData = new FormData();
-        formData.append('file', recordingBlob, 'recording.wav');
+        formData.append("file", recordingBlob, "recording.wav");
         try {
-          console.log('Sending audio to model...');
-          const response = await fetch('http://127.0.0.1:8092/predict/', {
-            method: 'POST',
+          console.log("Sending audio to model...");
+          const response = await fetch("http://127.0.0.1:8092/predict/", {
+            method: "POST",
             body: formData,
           });
-          setVerify(response);
           // Log response status
-          console.log('Response status:', response.status);
+          console.log("Response status:", response.status);
           // Check if the response status is not OK
           if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
           }
           const data = await response.json();
-          console.log('Response from backend:', data);
-          // if(data.predicted_speaker!=="other" && data.predicted_speaker!=="Cloned" && data.predicted_speaker!=="No Voice")
-          //   {
-          //     activateLogin();
-          //   }
+          console.log("Response from backend:", data);
+          setVerify(data);
+          if (
+            verify.predicted_speaker !== "other" &&
+            verify.predicted_speaker !== "Cloned" &&
+            verify.predicted_speaker !== "No Voice"
+          ) {
+            activateLogin();
+          }
         } catch (error) {
-          console.error('Error sending audio to backend:', error);
+          console.error("Error sending audio to backend:", error);
         }
-      } else {
-        console.log('No blob available to send.');
-      }
-      
-    };
-    sendAudioToBackend();
-    
+      };
 
+      sendAudioToBackend();
+    }
   }, [recordingBlob]);
 
-  function handleRetry()
-  {
+  useEffect(() => {
+    console.log("UseEffect called for Recording time => ", recordingTime);
+    if (recordingTime === 7) {
+      stopRecording();
+      console.log(" Recording Blob after 7 secs: ", recordingBlob);
+    }
+  }, [recordingTime]);
+
+  function handleRetry() {
     setMyRecording(false);
     setResult(false);
+    setVerify({});
+    
   }
-
- 
-  
 
   return (
     <>
-    
-      {(!isRecording && !myrecording && !result) &&<button className="start-record" onClick={startRecording} disabled={isRecording}>
-        <img src={micIcon} alt="" />
-        SPEAK FOR PASSWORD
-      </button>}
-      
-      {(isRecording && !myrecording && !result) && <button onClick={stopRecording} className="stop-record" disabled={!isRecording}>
-        <img src={stopIcon} alt="" />
-        <Waves />
-      </button>}
-      {/* <button onClick={togglePauseResume} disabled={!isRecording}>
-        {isPaused ? "Resume" : "Pause"}
-      </button> */}
+      {!isRecording && !myRecording && !result && (
+        <button className="start-record" onClick={startRecording} disabled={isRecording}>
+          <img src={micIcon} alt="" />
+          SPEAK FOR PASSWORD
+        </button>
+      )}
+
+      {isRecording && !myRecording && !result && (
+        <button onClick={stopRecording} className="stop-record" disabled={!isRecording}>
+          <img src={stopIcon} alt="" />
+          <Waves />
+        </button>
+      )}
       {isRecording && <div>Recording Time: {recordingTime}s</div>}
-      {(myrecording && result) && <VerifyPrompt mess={verify} retry={handleRetry}  />}
+      {myRecording && result && <VerifyPrompt mess={verify} retry={handleRetry} />}
     </>
   );
 };
 
 export default AudioRecorderComponent;
-
